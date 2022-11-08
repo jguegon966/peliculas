e274ca6b5b8d608e946f0f336366c4df02c0d3a7
jguegon966 <jguegon966@g.educaand.es>
2022-11-08 14:43:10 +0100

    Peliculas2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Peliculas en cines'),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.search_outlined))
+          ],
+        ),
+        body: Column(
+          children: const [
+            CardSwiper()
+            ],
+        ));
   }
 }
---
+++ lib/widgets/card_swiper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_card_swipper/flutter_card_swiper.dart';
+
+class CardSwiper extends StatelessWidget {
+  const CardSwiper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.5,
+      color: Colors.red,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.9,
+        itemBuilder: (_, int index) {
+          return const FadeInImage(
+              placeholder: AssetImage('assets/loading.gif'),
+              image: AssetImage('assets/no-image.jpg')
+          );
+        },
+      ),
+    );
+  }
+}
